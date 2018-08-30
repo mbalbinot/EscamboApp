a597bbd9656d70a88a0ceea0d2bb4bb951bbfbe4
Marcelo Balbinot <marc.balbinot@gmail.com>
2018-08-30 00:14:29 -0300

    Devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :backoffice do
     get 'dashboard', to: 'dashboard#index'
   end
+  get 'backoffice', to: 'backoffice/dashboard#index'
   devise_for :admins
   devise_for :members
   root 'site/home#index'
