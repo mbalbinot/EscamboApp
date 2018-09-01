b1c6da4d9d23b396ebf37d8a0ef1348f5c847075
Marcelo Balbinot <marc.balbinot@gmail.com>
2018-09-01 17:28:32 -0300

    Including Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
   namespace :backoffice do
-    get 'categories/index'
+    resources :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
   end
+
   namespace :site do
     get 'home', to: 'home#index'
   end
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
-  get 'backoffice', to: 'backoffice/dashboard#index'
+
   devise_for :admins
   devise_for :members
+
   root 'site/home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
